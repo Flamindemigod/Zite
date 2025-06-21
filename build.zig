cb10e33b0e56cb93d9e230f23b68dccb02641d6b
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2025-06-21 23:04:31 +0100

    removed main.zig and also commented out the executable sections in build.zig as i plan on using that later to compile some examples

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/Zite.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -12,13 +12,13 @@ pub fn build(b: *std.Build) void {
     lib_mod.addIncludePath(b.path("sqlite"));
     lib_mod.link_libc = true;
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // const exe_mod = b.createModule(.{
+    //     .root_source_file = b.path("src/main.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    exe_mod.addImport("Zite_lib", lib_mod);
+    // exe_mod.addImport("Zite_lib", lib_mod);
 
     const lib = b.addLibrary(.{
         .linkage = .static,
@@ -28,23 +28,23 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
-    const exe = b.addExecutable(.{
-        .name = "Zite",
-        .root_module = exe_mod,
-    });
+    // const exe = b.addExecutable(.{
+    //     .name = "Zite",
+    //     .root_module = exe_mod,
+    // });
+    //
+    // b.installArtifact(exe);
 
-    b.installArtifact(exe);
+    // const run_cmd = b.addRunArtifact(exe);
 
-    const run_cmd = b.addRunArtifact(exe);
+    // run_cmd.step.dependOn(b.getInstallStep());
 
-    run_cmd.step.dependOn(b.getInstallStep());
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    // const run_step = b.step("run", "Run the app");
+    // run_step.dependOn(&run_cmd.step);
 
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
@@ -52,13 +52,13 @@ pub fn build(b: *std.Build) void {
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
+    // const exe_unit_tests = b.addTest(.{
+    //     .root_module = exe_mod,
+    // });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    // test_step.dependOn(&run_exe_unit_tests.step);
 }
