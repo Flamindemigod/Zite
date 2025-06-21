e797d7494be6c7c3aff7c3414ec9c7daa61526b5
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2025-06-21 22:44:58 +0100

    Init Zite a SQLite Zig Binding Library

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Zite Dev Flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+    devShell.x86_64-linux = let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {inherit system;};
+    in
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gdb
+          zig
+          zls
+          ];
+        shellHook = ''
+          export OPT="Debug"
+          function build {
+              if [ -z "$1" ]
+              then
+                zig build -Doptimize=$OPT;
+              else
+                zig build -Doptimize=$OPT "$1";
+              fi
+          }
+          function check { zig build -Doptimize=$OPT --watch check;}
+          zig zen
+        '';
+      };
+  };
+}
