1d5d5c114682605d2e59811e38d4d87e6892d47a
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2025-06-28 11:47:57 +0100

    prepped the lib for export according to the idiot

--- build.zig
+++ build.zig
@@ -12,14 +12,6 @@ pub fn build(b: *std.Build) void {
     lib_mod.addIncludePath(b.path("sqlite"));
     lib_mod.link_libc = true;
 
-    // const exe_mod = b.createModule(.{
-    //     .root_source_file = b.path("src/main.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-
-    // exe_mod.addImport("Zite_lib", lib_mod);
-
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "Zite",
@@ -28,37 +20,22 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
-    // const exe = b.addExecutable(.{
-    //     .name = "Zite",
-    //     .root_module = exe_mod,
-    // });
-    //
-    // b.installArtifact(exe);
-
-    // const run_cmd = b.addRunArtifact(exe);
-
-    // run_cmd.step.dependOn(b.getInstallStep());
-
-    // if (b.args) |args| {
-    //     run_cmd.addArgs(args);
-    // }
-
-    // const run_step = b.step("run", "Run the app");
-    // run_step.dependOn(&run_cmd.step);
-
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    // const exe_unit_tests = b.addTest(.{
-    //     .root_module = exe_mod,
-    // });
-
-    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    // test_step.dependOn(&run_exe_unit_tests.step);
+}
+
+pub fn module(b: *std.Build) *std.Build.Module {
+    const pubMod = b.addModule("Zite", .{
+        .root_source_file = b.path("src/Zite.zig"),
+        .link_libc = true,
+    });
+    pubMod.addCSourceFile(.{ .file = b.path("sqlite/sqlite3.c") });
+    pubMod.addIncludePath(b.path("sqlite"));
+    return pubMod;
 }
